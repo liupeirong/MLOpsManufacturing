18dd43c45418eeb621fb4f8e103ab910b0123661
Paige <paige@microsoft.com>
2020-11-05 04:16:09 +0000

    lint

--- ml_model/preprocessing/preprocess_aml.py
+++ ml_model/preprocessing/preprocess_aml.py
@@ -109,7 +109,7 @@ def main():
         run.parent.log(k, v)
 
     # Link dataset to the step run so it is trackable in the UI
-    # run input_datasets is not registered. Bug https://docs.microsoft.com/en-us/answers/questions/114019/registered-dataset-is-not-logged-as-reference-in-a.html.  # NOQA: E501 
+    # run input_datasets is not registered. Bug https://docs.microsoft.com/en-us/answers/questions/114019/registered-dataset-is-not-logged-as-reference-in-a.html.  # NOQA: E501
     run.input_datasets['flower_dataset_raw'] = dataset
     run.parent.input_datasets['flower_dataset_raw'] = dataset
 
@@ -117,7 +117,7 @@ def main():
     mount_context = dataset.mount()
     mount_context.start()
     print(f"mount_point is: {mount_context.mount_point}")
-    resize_images(mount_context.mount_point, output_dataset, preprocessing_args)
+    resize_images(mount_context.mount_point, output_dataset, preprocessing_args)  # NOQA: E501
     mount_context.stop()
 
     run.tag("run_type", value="preprocess")

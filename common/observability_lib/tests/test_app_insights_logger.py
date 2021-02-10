ae54263afe748360ad2a3cf227a4e0661c11154e
Paige <paige@microsoft.com>
2021-02-10 04:56:15 +0000

    opencensus mock

--- common/observability_lib/tests/test_app_insights_logger.py
+++ common/observability_lib/tests/test_app_insights_logger.py
@@ -1,7 +1,9 @@
-from src.app_insights_logger import AppInsightsLogger
+from src.app_insights_logger import AppInsightsLogger, logging
+import uuid
+from opencensus.trace.span import SpanKind
 
 
-def test_get_run_id_having_online_context(mocker):
+def test_get_run_id_should_use_runid_in_online_run(mocker):
     mock_run = mocker.MagicMock()
     mock_run.id = 'FOO'
     mock_run.name = 'FOO1'
@@ -31,7 +33,7 @@ def test_get_run_id_having_online_context(mocker):
     assert actual_custom_dimensions == expected_custom_dimensions
 
 
-def test_get_run_id_having_online_context_using_buildid(mocker):
+def test_get_run_id_should_use_buildid_in_offline_run(mocker):
     # arrange
     mock_run = mocker.MagicMock()
     mock_run.id = 'OfflineRun'
@@ -59,53 +61,94 @@ def test_get_run_id_having_online_context_using_buildid(mocker):
     assert actual_custom_dimensions == expected_custom_dimensions
 
 
-# def test_constructor():
-#     pass
+def test_get_run_id_should_use_uuid_in_offline_run_when_no_buildid(mocker):
+    # arrange
+    mock_run = mocker.MagicMock()
+    mock_run.id = 'OfflineRun'
+    expected_run_id = "BAR"
+    mocker.patch(
+        'src.app_insights_logger.Env.build_id',
+        new_callable=mocker.PropertyMock,
+        return_value=None
+    )
+    mocker.patch.object(uuid, 'uuid1', return_value=expected_run_id)
+
+    expected_custom_dimensions = {
+        'custom_dimensions': {
+            "run_id": expected_run_id,
+            "offline_run": True
+        }
+    }
+
+    # action
+    logger = AppInsightsLogger(mock_run)
+    actual_run_id = logger.run_id
+    actual_custom_dimensions = logger.custom_dimensions
+
+    # logger.custom_dimensions
+    assert actual_run_id == expected_run_id
+    assert actual_custom_dimensions == expected_custom_dimensions
 
 
-# class RealAppInsightsLogger(AppInsightsLogger):
-#     def __init__(self):
-#         self.logger = logging.getLogger(__name__)
-#         self.env = MockEnv("")
-#         self.tracer = Tracer()
+def test_opencensus_init_with_env_vars(mocker):
+    # arrange
+    mock_run = mocker.MagicMock()
+    mock_run.id = 'OfflineRun'
+    mock_exporter = mocker.patch(
+        'src.app_insights_logger.AzureExporter', autospec=True)
+    expected_connection_string = 'FOO'
+    mock_sampler = mocker.patch(
+        'src.app_insights_logger.ProbabilitySampler', autospec=True)
+    expected_sampling_rate = 0.7
+    mock_logger = mocker.patch.object(logging, 'getLogger')
+    mock_setLevel = mock_logger.return_value.setLevel
+    expected_log_level_string = 'ERROR'
+    expected_log_level = getattr(logging, expected_log_level_string)
+
+    mocker.patch('src.app_insights_logger.AzureLogHandler', autospec=True)
+    mocker.patch('src.app_insights_logger.metrics_exporter', autospec=True)
+    mocker.patch(
+        'src.app_insights_logger.Env.app_insights_connection_string',
+        new_callable=mocker.PropertyMock,
+        return_value=expected_connection_string
+    )
+    mocker.patch(
+        'src.app_insights_logger.Env.trace_sampling_rate',
+        new_callable=mocker.PropertyMock,
+        return_value=expected_sampling_rate
+    )
+    mocker.patch(
+        'src.app_insights_logger.Env.log_level',
+        new_callable=mocker.PropertyMock,
+        return_value=expected_log_level_string
+    )
+
+    # act
+    AppInsightsLogger(mock_run)
+
+    # assert
+    mock_exporter.assert_called_once_with(
+        connection_string=expected_connection_string)
+    mock_sampler.assert_called_once_with(
+        expected_sampling_rate)
+    mock_setLevel.assert_called_once_with(
+        expected_log_level)
 
 
-# class MockEnv:
-#     def __init__(self, run_id):
-#         self.build_id = run_id
+def test_start_span(mocker):
+    # arrange
+    mock_tracer = mocker.patch(
+        'src.app_insights_logger.Tracer', autospec=True)
+    mock_run = mocker.MagicMock()
+    mock_run.id = 'OfflineRun'
+    span_name = 'FOO'
 
+    # act
+    logger = AppInsightsLogger(mock_run)
+    span = logger.start_span(span_name)
 
-# class MockSpan:
-#     def __init__(self, name):
-#         self.span_kind = None
-#         self.attributes = {}
-
-
-# class TestObservability(unittest.TestCase):
-#     @patch("src.app_insights_logger.AppInsightsLogger")
-#     def setUp(self, mock_app_insights_logger):
-#         self.concert_app_insights_logger = RealAppInsightsLogger()
-#         self.mock_app_insights_logger = mock_app_insights_logger
-
-#     def test_get_run_id_having_online_context_using_uuid(self):
-#         self.concert_app_insights_logger.env.build_id = ""
-
-#         response = self.concert_app_insights_logger.\
-#             get_run_id_and_set_context(MockRun("OfflineRun"))
-
-#         self.assertIsNotNone(response)
-
-
-#     @patch.object(Tracer, "start_span")
-#     def test_start_span_having_online_context(self, mock_tracer):
-#         name = "foo"
-#         mock_tracer.return_value = MockSpan(name)
-#         test_span = self.concert_app_insights_logger.start_span(name)
-#         mock_tracer.assert_called_with(name)
-#         self.assertEqual(test_span.span_kind, SpanKind.SERVER)
-#         self.assertEqual(test_span.attributes['http.method'], 'START')
-#         self.assertEqual(test_span.attributes['http.route'], name)
-
-
-# if __name__ == "__main__":
-#     unittest.main()
+    # assert
+    mock_tracer.return_value.start_span.assert_called_once_with(span_name)
+    assert span.span_kind == SpanKind.SERVER
+    span.attributes.__setitem__.assert_any_call('http.route', span_name)
+    span.attributes.__setitem__.assert_any_call('http.method', 'START')

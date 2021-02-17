97ba4a1b594f14cdaef17f1338ad59f5519c6959
angie4u <eunk.angie@outlook.com>
2021-02-17 07:23:38 +0000

    Fix linting error

--- samples/non-python-preprocess/ml_service/tests/pipelines/test_run_data_processing_pipeline.py
+++ samples/non-python-preprocess/ml_service/tests/pipelines/test_run_data_processing_pipeline.py
@@ -1,14 +1,7 @@
-import sys
 import runpy
 from pytest_mock import MockFixture
 from easydict import EasyDict as edict
 from ml_service.tests.pipelines.test_aml_mock_fixtures_env import environment_vars, aml_pipeline_mocks  # NOQA: F401, E501
-from azureml.pipeline.core import PublishedPipeline
-from azureml.pipeline.core import Pipeline
-from azureml.core import Experiment
-import argparse
-
-
 
 
 def test_run_data_processing_pipeline_happy_path(mocker: MockFixture,
@@ -22,10 +15,12 @@ def test_run_data_processing_pipeline_happy_path(mocker: MockFixture,
         aml_pipeline_mocks
 
     mock_args = mocker.patch('argparse.ArgumentParser.parse_args')
-    mock_args.return_value = edict({"aml_pipeline_name": "aml_pipeline_name_test", "output_pipeline_id_file": "w", "skip_preprocessing_execution": False})
+    mock_args.return_value = edict({"aml_pipeline_name": "aml_pipeline_name_test",  # NOQA: E501
+                                    "output_pipeline_id_file": "w",
+                                    "skip_preprocessing_execution": False})
 
-    mock_aml_pipelines_list = mocker.patch('azureml.pipeline.core.PublishedPipeline.list')
-    type(mock_pipeline_publish).name = mocker.PropertyMock(return_value='aml_pipeline_name_test')
+    mock_aml_pipelines_list = mocker.patch('azureml.pipeline.core.PublishedPipeline.list')  # NOQA: E501
+    type(mock_pipeline_publish).name = mocker.PropertyMock(return_value='aml_pipeline_name_test')  # NOQA: E501
     type(mock_pipeline_publish).id = mocker.PropertyMock(return_value='1')
     mock_aml_pipelines_list.return_value = [mock_pipeline_publish]
     mocker.patch('azureml.core.Experiment')
@@ -34,4 +29,3 @@ def test_run_data_processing_pipeline_happy_path(mocker: MockFixture,
     runpy.run_module('ml_service.pipelines.'
                      'run_data_processing_pipeline',
                      run_name='__main__')
-
